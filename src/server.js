66c4fd353dab0d29c2a0d7c459b992ef9ec529f5
Jayaram <121944470+JayaramHarry@users.noreply.github.com>
2024-01-18 16:20:32 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -22,6 +22,6 @@ const errorHandler = (err, req, res, next) => {
 
 app.use(errorHandler);
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(process.env.PORT || 3001, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
